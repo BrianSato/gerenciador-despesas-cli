55efe26b794ed440dd0dcb0520de043cb918c261
Brian <brian@google.com>
2026-02-08 10:37:58 +0900

    Padroniza tela Menu e melhora UX

--- gui/telas/tela_menu.py
+++ gui/telas/tela_menu.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from gui.config.estilos import *
 
 class TelaMenu(ttk.Frame):
     def __init__(self,parent,controller):
@@ -8,19 +9,22 @@ class TelaMenu(ttk.Frame):
         self._configurar_grid()
         self._criar_widgets()
 
-    def _criar_widgets(self):
-
-        tk.Label(self, text="Menu Principal", font=("Arial", 14),anchor="center").grid(row=0,column=0,pady=15)
-
-        ttk.Button(self, text="Adicionar Despesa", command=lambda: self.controller.mostrar_tela("adicionar")
-        ).grid(row=1,column=0,pady=5)
-        ttk.Button(self, text="Listar Despesa", command=lambda: self.controller.mostrar_tela("listar")
-        ).grid(row=2,column=0,pady=5)
-        ttk.Button(self, text="Filtrar Despesa", command=lambda: self.controller.mostrar_tela("filtro")
-        ).grid(row=3,column=0,pady=5)
-        ttk.Button(self, text="Estatísticas", command=lambda :self.controller.mostrar_tela("filtro_estatistica")
-        ).grid(row=4,column=0,pady=5)
-
     def _configurar_grid(self):
-        self.grid_columnconfigure(0,weight=0)
-        self.grid_columnconfigure(1,weight=1)
+        self.grid_columnconfigure(0,weight=1)
+
+    def _criar_widgets(self):
+        #SubTitulo
+        ttk.Label(self, text="Menu Principal", font=(FONTE_SUBTITULO)
+        ).grid(row=0,column=0,pady=(PADY_PADRAO_SUBTITULO),sticky="n")
+
+        #Botoes
+        botoes = [
+            ("Adicionar Despesa","adicionar"),
+            ("Listar Despesa", "listar"),
+            ("Filtrar Despesa","filtro"),
+            ("Estatisticas","filtro_estatistica")
+        ]
+        for i, (texto,tela) in enumerate(botoes,start=1):
+            ttk.Button(self, text=texto,width=(WIDGET_PADRAO), command=lambda t=tela: self.controller.mostrar_tela(t)
+            ).grid(row=i,column=0,pady=(PADY_BOTAO))
+
