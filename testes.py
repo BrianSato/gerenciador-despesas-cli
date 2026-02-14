3b5cde245dad5d10c7c26d660c95bb682a0a090c
Brian <brian@google.com>
2026-02-14 20:08:19 +0900

    Adiciona .gitignore para ifnorar arquivos de build

--- testes.py
+++
@@ -1,8 +0,0 @@
-from core.despesas_listar_core import listar_despesas
-from core.despesas_arquivo_core import  carregar_despesas
-
-despesas = carregar_despesas()
-
-print("DEBUG:", despesas)
-print(listar_despesas(despesas))
-
